128b489e8d5b41dfbf465a09587b39fa4283afbc
Alcides Algarin <algarinalcides@gmail.com>
2022-02-05 13:29:56 -0300

    TP 2 y 3

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -1,25 +1,77 @@
-/*2.	Para el departamento de Construcción:
-A.	 Mostrar la cantidad de alambre a comprar  si se ingresara 
-el largo y el ancho de un terreno rectangular y se debe alambra 
-con tres hilos de alambre.
+/*
+TP 2.
 
-B.	 Mostrar la cantidad de alambre a comprar  si se ingresara 
-el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
+Enunciado:
+Para el departamento de Construcción:
+    A. mostrar la cantidad de alambre a comprar si se 
+    ingresara el largo y el ancho de un terreno rectangular 
+    y se debe alambra con tres hilos de alambre.
 
-C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de 
-cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan 
-de cada uno para las medidas que nos ingresen.
+    B. mostrar la cantidad de alambre a comprar si se 
+    ingresara el radio de un terreno circular y se debe 
+    alambra con tres hilos de alambre.
+
+    C. Para hacer un contrapiso de 1m x 1m se necesitan 
+    2 bolsas de cemento y 3 de cal, debemos mostrar cuantas 
+    bolsas se necesitan de cada uno para las medidas del 
+    terreno rectangular.
+
+    https://onlinegdb.com/k2SmKeV4e
 */
+let largo;
+let ancho;
+let radio;
+let perimetro;
+let superficie;
+
 
 function Rectangulo () 
 {
+    largo = document.getElementById("txtIdLargo").value;
+    ancho = document.getElementById("txtIdAncho").value;
+
+    largo = parseFloat(largo);
+    ancho = parseFloat(ancho);
+
+    perimetro = largo * 2 + ancho * 2;
+
+    alert("Necesitara " + perimetro*3 + " metros de alambre para el terreno rectangular.");
 
 }
+
+
 function Circulo () 
 {
-	
+
+    radio = document.getElementById("txtIdRadio").value;
+
+    radio = parseFloat(radio);
+
+    perimetro = 3.14 * radio;
+
+    alert("Necesitara " + perimetro*3 + " metros de alambre para el terreno circular.");
+    	
 }
+
+
 function Materiales () 
 {
-	
+
+    let totalCemento;
+    let totalCal;
+
+    largo = document.getElementById("txtIdLargo").value;
+    ancho = document.getElementById("txtIdAncho").value;
+
+    largo = parseFloat(largo);
+    ancho = parseFloat(ancho);
+
+    superficie = largo * ancho;
+
+    totalCemento = (superficie * 2)/1;
+
+    totalCal = (superficie * 3)/1;
+
+    alert("Se necesitara " + totalCemento + " bolsas de Cto y " + totalCal + " bolsas de cal.");
+
 }
--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -1,13 +1,38 @@
-/*3.	Para el departamento de Pinturas:
-A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
-B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
+/*
+Enunciado:
+3. Para el departamento de Pinturas:
+
+    A. Al ingresar una temperatura en Fahrenheit debemos 
+    mostrar la temperatura en Centígrados con un mensaje 
+    concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
+
+    B. Al ingresar una temperatura en Centígrados debemos 
+    mostrar la temperatura en Fahrenheit (ej.: "0 centígrados 
+    son 32 Fahrenheit "). 
+
+    https://onlinegdb.com/nFfcMJHVE
 */
+
+let temperatura;
+let gradosFahrenheit;
+let gradosCentigrados;
+
 function FahrenheitCentigrados () 
 {
+    temperatura = document.getElementById("txtIdTemperatura").value;
+    temperatura = parseFloat(temperatura);
+
+    gradosCentigrados = (temperatura - 32) * 5/9;
 	
+    alert(temperatura + "° Fahrenheit equivalen a " + gradosCentigrados + "° centigrados.");
 }
 
 function CentigradosFahrenheit () 
 {
+    temperatura = document.getElementById("txtIdTemperatura").value;
+    temperatura = parseFloat(temperatura);
+
+    gradosFahrenheit = 9/5 * temperatura + 32;
 	
+    alert(temperatura + "° centigrados equivalen a " + gradosFahrenheit + "° Fahrenheit.");	
 }
